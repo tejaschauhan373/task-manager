83b8766500f62c34b607956649bd08dfb51f4b9f
Tejas Chauhan <tejas.chauhan@kevit.io>
2021-05-13 16:59:12 +0530

    fix file format

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const userRouter = require('./router/user');
+const taskRouter = require('./router/task');
+const app = express();
+app.use(express.json());
+
+app.use(userRouter);
+app.use(taskRouter);
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const userRouter = require('./router/user');
-const taskRouter = require('./router/task');
-const app = express();
-app.use(express.json());
-
-app.use(userRouter);
-app.use(taskRouter);
+const app = require('app')
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
