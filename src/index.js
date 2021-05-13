d93c1f2c45ac5cae87816cebbbfdc5218c565ff5
Tejas Chauhan <tejas.chauhan@kevit.io>
2021-05-13 11:59:44 +0530

    fix port number

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const userRouter = require('./router/user');
 const taskRouter = require('./router/task');
 const app = express();
-const port = process.env.TASK_MANAGER_APP_PORT || 3000;
 app.use(express.json());
 
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port);
-})
+app.listen(process.env.TASK_MANAGER_APP_PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
