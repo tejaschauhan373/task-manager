181260707fe1f6e4fbc9d4266b200f6480659798
Tejas Chauhan <tejas.chauhan@kevit.io>
2021-05-13 11:48:22 +0530

    set port number

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRouter = require('./router/user');
 const taskRouter = require('./router/task');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.TASK_MANAGER_APP_PORT || 3000;
 app.use(express.json());
 
 app.use(userRouter);
